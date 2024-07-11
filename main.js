42c1efc050cb6f89f4c885269d432918168979a8
Loice Khoene <82638055+Loicemary@users.noreply.github.com>
2024-07-11 05:53:58 +0300

    Adding  assets

--- main.js
+++ main.js
@@ -19,4 +19,50 @@ const navSearch = document.getElementById("nav-search");
 
 navSearch.addEventListener("click", (e) => {
   navSearch.classList.toggle("open");
-})
+});
+
+const scrollRevealOption =  {
+  distance: "50px",
+  origin: "bottom",
+  duration: "1000", 
+};
+
+ScrollReveal().reveal(".header_image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header_content div", {
+  duration: 1000,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header_content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header_content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".deals_card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".about_image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".about_card", {
+  duration: 1000,
+  interval:500,
+  delay: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop:true,
+});
